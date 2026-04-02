894a46b392866e03d8a5dc9233c0fd5dde339c57
rj0141 <jain.rohit01@gmail.com>
2026-04-02 19:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let lastDetected = Date.now();
 const TIMEOUT = 30000; // 30 seconds timeout
 
 // Initialize Mediapipe Pose (CDN version)
-const pose = new Pose.Pose({
+const pose = new pose.Pose({
   locateFile: file =>
     `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`
 });
